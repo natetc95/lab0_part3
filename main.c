ea68bc2d86d94e0d04db8772f7ae56c2828b7815
natetc95 <natetc95@email.arizona.edu>
2016-02-01 13:30:50 -0700

    Lab0_part3 Version 2
    Updated to use the button on the board. able to recognize the difference
    between long and short presses.

--- main.c
+++ main.c
@@ -24,19 +24,22 @@ int main(void) {
     LATDbits.LATD1 = 0;
     LATDbits.LATD2 = 0;
     //Timer Initialization
-    PR1 = 2441;
+    PR1 = 100000;
     TMR1 = 0;
     T1CONbits.TCKPS = 3;
     IEC0bits.T1IE = 1;
     IFS0bits.T1IF = 0;
-    IPC1bits.T1IP = 5;
+    IPC1bits.T1IP = 2;
     T1CONbits.ON = 1;
     //Switch Initialization
     TRISDbits.TRISD6 = 1;
     CNCONDbits.ON = 1;
     CNENDbits.CNIED6 = 1;
-    IFS1bits.CNDIF = 0;
     CNPUDbits.CNPUD6 = 1;
+    IFS1bits.CNDIF = 0;
+    IPC8bits.CNIP = 4;
+    IPC8bits.CNIS = 3;
+    IEC1bits.CNDIE = 1;
     while(1) {
         switch(stat) {
             case led1:
@@ -46,13 +49,13 @@ int main(void) {
                 break;
             case led2:
                 LATDbits.LATD0 = 0;
-                LATDbits.LATD1 = 1;
-                LATDbits.LATD2 = 0;
+                LATDbits.LATD1 = 0;
+                LATDbits.LATD2 = 1;
                 break;
             case led3:
                 LATDbits.LATD0 = 0;
-                LATDbits.LATD1 = 0;
-                LATDbits.LATD2 = 1;
+                LATDbits.LATD1 = 1;
+                LATDbits.LATD2 = 0;
                 break;
             default:
                 LATDbits.LATD0 = 0;
@@ -64,9 +67,21 @@ int main(void) {
     return 0;
 }
 
-void __ISR(_TIMER_1_VECTOR, IPL5SRS) T1Interrupt() {
-    IFS0bits.T1IF = 0;
-    if(stat == led1) stat = led3;
-    else if(stat == led2) stat = led1;
-    else if(stat == led3) stat = led2;
+void __ISR(_CHANGE_NOTICE_VECTOR, IPL4SRS) _CNInterrupt( void ){
+    IFS1bits.CNDIF = 0;
+    if(PORTDbits.RD6 == 0) {
+        TMR1 = 0;
+    }
+    if(PORTDbits.RD6 == 1) {
+        if(TMR1 < 2441) {
+            if(stat == led1) stat = led2;
+            else if(stat == led2) stat = led3;
+            else if(stat == led3) stat = led1;
+        }
+        else {
+            if(stat == led1) stat = led3;
+            else if(stat == led2) stat = led1;
+            else if(stat == led3) stat = led2;
+        }
+    }
 }
